062d4837f5c2ec71480fc376cb78f3610715f4b5
Andrew Davis <atd285@gmail.com>
2019-12-28 21:43:42 -0500

    bring in vue composition api

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Is it new years day yet
 
 ## A countdown application informing the user of how far away new years day is
+
+- Front end - Vue
+- Back-end - Node.js + express + Socket.io
+
+## Todo
+
+- Dark/Light Theme
--- client/src/components/Countdown.vue
+++ client/src/components/Countdown.vue
@@ -1,14 +1,48 @@
 <template>
   <div>
-    <p>NO</p>
-    <p>3 days till New Years Day</p>
+    <p class="yes-no">NO</p>
+    <p class="time-left">{{ currentTime }}</p>
   </div>
 </template>
 
 <script>
+// import { formatDistance } from 'date-fns';
+import { ref } from '@vue/composition-api';
+
 export default {
+  // data: () => ({
+  //   currentTime: new Date().toLocaleString(),
+  // }),
+  // created() {
+  //   setInterval(() => {
+  //     this.currentTime = new Date().toLocaleString();
+  //   }, 500);
+  // },
   name: 'Countdown',
+  setup() {
+    const currentTime = ref(new Date().toLocaleString());
+    const today = new Date();
+    // eslint-disable-next-line no-unused-vars
+    const newYearsDay = new Date(today.getFullYear() + 1, 1, 1);
+    setInterval(() => {
+      currentTime.value = new Date().toLocaleString();
+    }, 500);
+    return {
+      currentTime,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.yes-no {
+  margin: 2rem;
+  text-align: center;
+  font-size: calc(2rem + 8vmin);
+}
+.question,
+.time-left {
+  text-align: center;
+  font-size: calc(1rem + 4vmin);
+}
+</style>
